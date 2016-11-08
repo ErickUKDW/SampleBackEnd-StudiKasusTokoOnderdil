d8d804ee452cf45f9fc59daa5fc7d81c269b32c7
erick <erick@DESKTOP-I963S2R>
2016-11-08 10:13:58 +0700

    menambahkan JenisMotorController

---
+++ SampleBackEnd/Controllers/JenisMotorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleBackEnd.Controllers
+{
+    public class JenisMotorController : ApiController
+    {
+        // GET: api/JenisMotor
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/JenisMotor/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/JenisMotor
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/JenisMotor/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/JenisMotor/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
