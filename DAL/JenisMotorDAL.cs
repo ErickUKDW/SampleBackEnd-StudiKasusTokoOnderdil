99a76f93b3f4293c53410782e35e05bac4524675
erick <erick@DESKTOP-I963S2R>
2016-11-08 10:01:10 +0700

    membuat connection string

--- DAL/JenisMotorDAL.cs
+++ DAL/JenisMotorDAL.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+using BO;
+using Dapper;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DAL
 {
     public class JenisMotorDAL
     {
+        private string GetConnStr()
+        {
+            return ConfigurationManager.ConnectionStrings["StokDbConnectionString"].ConnectionString;
+        }
     }
 }
