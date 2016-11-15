8bb1a69f0c790d0e43f2a130d7e1f54c035a263d
erick <erick@DESKTOP-I963S2R>
2016-11-15 08:47:06 +0700

    menambahkan KategoriDAL

---
+++ DAL/KategoriDAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BO;
+using Dapper;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    public class KategoriDAL
+    {
+        private string GetConnStr()
+        {
+            return ConfigurationManager.ConnectionStrings["StokDbConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Kategori> GetAll()
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+               
+                string strSql = @"select * from Kategori 
+                                  order by NamaKategori";
+
+                return conn.Query<Kategori>(strSql);
+
+                /*List<Kategori> lstKategori = new List<Kategori>();
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        lstKategori.Add(new Kategori
+                        {
+                            KategoriId = Convert.ToInt32(dr["KategoriId"]),
+                            NamaKategori = dr["NamaKategori"].ToString()
+                        });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+
+                return lstKategori;*/
+            }
+        }
+
+
+    }
+}
